04a27c2dcbb02d398063ff2456eb6dec020d54da
Jonathan Terrell <terrell.jm@gmail.com>
2021-07-15 18:23:25 +0200

    update

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log(1234);
-console.log("abcde");
+export const test1 = 12345;
+export const test2 = "abcde";
+
+export function draw(ctx, length, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, length, length);
+
+  return {
+    length: length,
+    x: x,
+    y: y,
+    color: color,
+  };
+}
