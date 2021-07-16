cdc42a35589917422527789a10cb89ca725e9bba
Jonathan Terrell <terrell.jm@gmail.com>
2021-07-16 10:30:28 +0200

    update

---
+++ src/index.js
@@ -0,0 +1,14 @@
+export const test1 = 12345;
+export const test2 = "abcde";
+
+export function draw(ctx, length, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, length, length);
+
+  return {
+    length: length,
+    x: x,
+    y: y,
+    color: color,
+  };
+}
