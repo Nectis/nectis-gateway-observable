dc13c7331fc631aa7457eb3c75c79a50f5c5d854
Jonathan Terrell <terrell.jm@gmail.com>
2021-07-16 11:07:09 +0200

    Release v0.0.6

--- gruntfile.js
+++ gruntfile.js
@@ -9,53 +9,47 @@
 // -------------------------------------------------------------------------------------------------------------------------------
 
 module.exports = (grunt) => {
-  // Initialise configuration.
-  grunt.initConfig({
-    // Bump configuration.
-    bump: {
-      options: {
-        commitFiles: ["-a"],
-        commitMessage:
-          '<%if(grunt.config("commitMessage")){%><%=grunt.config("commitMessage")%><%}else{%>Release v%VERSION%<%}%>',
-        pushTo: "origin",
-      },
-    },
+    // Initialise configuration.
+    grunt.initConfig({
+        // Bump configuration.
+        bump: {
+            options: {
+                commitFiles: ['-a'],
+                commitMessage:
+                    '<%if(grunt.config("commitMessage")){%><%=grunt.config("commitMessage")%><%}else{%>Release v%VERSION%<%}%>',
+                pushTo: 'origin'
+            }
+        },
 
-    // Run configuration.
-    run: {
-      audit: { args: ["npm", "audit"], cmd: "npx" },
-      install: { args: ["npm", "install"], cmd: "npx" },
-      licenseChecker: {
-        args: [
-          "license-checker",
-          "--production",
-          "--json",
-          "--out",
-          "LICENSES.json",
-        ],
-        cmd: "npx",
-      },
-      licenseNLF: { args: ["nlf", "-d"], cmd: "npx" },
-      lint: { args: ["eslint", "src/**/*.js"], cmd: "npx" },
-      outdated: { args: ["npm", "outdated"], cmd: "npx" },
-      publish: { args: ["publish"], cmd: "npx" },
-      rollup: { args: ["rollup", "-c"], cmd: "npx" },
-      test: { args: ["WARNING: No tests implemented."], cmd: "echo" },
-      update: { args: ["npm-check-updates", "-u"], cmd: "npx" },
-    },
-  });
-  // Load external tasks.
-  grunt.loadNpmTasks("grunt-bump");
-  grunt.loadNpmTasks("grunt-run");
+        // Run configuration.
+        run: {
+            audit: { args: ['npm', 'audit'], cmd: 'npx' },
+            install: { args: ['npm', 'install'], cmd: 'npx' },
+            licenseChecker: {
+                args: ['license-checker', '--production', '--json', '--out', 'LICENSES.json'],
+                cmd: 'npx'
+            },
+            licenseNLF: { args: ['nlf', '-d'], cmd: 'npx' },
+            lint: { args: ['eslint', 'src/**/*.js'], cmd: 'npx' },
+            outdated: { args: ['npm', 'outdated'], cmd: 'npx' },
+            publish: { args: ['publish'], cmd: 'npx' },
+            rollup: { args: ['rollup', '-c'], cmd: 'npx' },
+            test: { args: ['WARNING: No tests implemented.'], cmd: 'echo' },
+            update: { args: ['npm-check-updates', '-u'], cmd: 'npx' }
+        }
+    });
+    // Load external tasks.
+    grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-run');
 
-  // Register local tasks.
-  grunt.registerTask("audit", ["run:audit"]);
-  grunt.registerTask("build", ["run:rollup"]);
-  grunt.registerTask("licenseCheck", ["run:licenseChecker", "run:licenseNLF"]);
-  grunt.registerTask("lint", ["run:lint"]);
-  grunt.registerTask("outdated", ["run:outdated"]);
-  grunt.registerTask("release", ["run:rollup", "bump", "run:publish"]);
-  grunt.registerTask("test", ["run:test"]);
-  grunt.registerTask("sync", ["bump"]);
-  grunt.registerTask("update", ["run:update", "run:install"]);
+    // Register local tasks.
+    grunt.registerTask('audit', ['run:audit']);
+    grunt.registerTask('build', ['run:rollup']);
+    grunt.registerTask('licenseCheck', ['run:licenseChecker', 'run:licenseNLF']);
+    grunt.registerTask('lint', ['run:lint']);
+    grunt.registerTask('outdated', ['run:outdated']);
+    grunt.registerTask('release', ['run:rollup', 'bump', 'run:publish']);
+    grunt.registerTask('test', ['run:test']);
+    grunt.registerTask('sync', ['bump']);
+    grunt.registerTask('update', ['run:update', 'run:install']);
 };
