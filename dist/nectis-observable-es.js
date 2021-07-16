ce9aefae3fc10fd14809aa717fc466fbd0a1cab4
Jonathan Terrell <terrell.jm@gmail.com>
2021-07-16 19:57:56 +0200

    Release v0.0.45

--- dist/nectis-observable-es.js
+++ dist/nectis-observable-es.js
@@ -121,48 +121,33 @@ class Table {
         const data = options.data;
         const columns = options.columns;
 
-        // const wrapper = html`<div style="overflow-x: scroll; padding: 10px 0 10px 10px"></div>`;
         const wrapper = document.createElement('div');
         wrapper.style.cssText = 'overflow-x: scroll; padding: 10px 0 10px 10px';
-        // const tableWrapper = html`<div style="display: flex"></div>`;
         const tableWrapper = document.createElement('div');
         tableWrapper.style.cssText = 'display: flex';
-        // const table = html`<table style="flex: 1 1 auto"></table>`;
         const table = document.createElement('table');
         table.style.cssText = 'flex: 1 1 auto';
-        // const tableRightPadding = html`<div style="flex: 0 0 10px"></div>`; // Implements padding on right.
         const tableRightPadding = document.createElement('div'); // Implements padding on right.
         tableRightPadding.style.cssText = 'flex: 0 0 10px';
 
-        //const header = html`<tr></tr>`;
         const header = document.createElement('tr');
         for (const column of columns) {
-            // header.appendChild(html`<th${buildCellStyle(column)}>${column.label}</th>`);
             const th = document.createElement('th');
             th.style.cssText = buildCellStyle(column);
-            const text = document.createTextNode(column.label.replaceAll('<br>', '\n'));
+            const text = document.createTextNode(column.label);
             th.append(text);
             header.appendChild(th);
         }
         table.appendChild(header);
 
         for (const record of data) {
-            // const row = html`<tr></tr>`;
             const row = document.createElement('tr');
             for (const column of columns) {
                 const td = document.createElement('td');
                 let text;
                 if (typeof column.source === 'function') {
-                    // row.appendChild(html`
-                    //     <td${buildCellStyle(column)}>
-                    //     ${formatCellValue(column, column.source(record, column))}
-                    //     </td>`);
                     text = document.createTextNode(formatCellValue(column, column.source(record, column)));
                 } else {
-                    // row.appendChild(html`
-                    //     <td${buildCellStyle(column)}>
-                    //     ${formatCellValue(column, record[column.source])}
-                    //     </td>`);
                     text = document.createTextNode(formatCellValue(column, record[column.source]));
                 }
                 td.appendChild(text);
@@ -174,7 +159,7 @@ class Table {
         tableWrapper.appendChild(table);
         tableWrapper.appendChild(tableRightPadding);
         wrapper.appendChild(tableWrapper);
-        replaceContent(element, wrapper);
+        element.replaceChildren(wrapper);
     }
 }
 
@@ -223,10 +208,6 @@ const formatCellValue = (column, value) => {
         default:
             return value.toLocaleString();
     }
-};
-
-const replaceContent = (element, content) => {
-    element.replaceChildren(content);
 };
 
 export { ChartJS, Table$1 as Table };
