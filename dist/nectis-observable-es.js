2b81176cd446c950e2b63b9b0cb7964bd923c7e8
Jonathan Terrell <terrell.jm@gmail.com>
2021-08-10 13:17:06 +0200

    Release v0.0.224

--- dist/nectis-observable-es.js
+++ dist/nectis-observable-es.js
@@ -1,8 +1,4 @@
-export { C as ChartJS, a as ChartPanel, D as D3XYCharts, E as ECharts, H as Highcharts, N as Nivo, T as TabPanel, b as Table, d as Theme, e as Tile, W as WorkforceSize } from './nectis-observable-index-e1d3baf7-es.js';
-import '@nivo/axes';
+export { C as ChartJS, a as ChartPanel, D as D3XYCharts, E as ECharts, H as Highcharts, N as Nivo, T as TabPanel, b as Table, d as Theme, e as Tile, W as WorkforceSize } from './nectis-observable-index-6560d662-es.js';
 import '@nivo/annotations';
 import '@nivo/legends';
-import '@nivo/core';
-import '@react-spring/web';
-import '@nivo/tooltip';
 import '@nivo/colors';
