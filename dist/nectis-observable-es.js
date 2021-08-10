2520abd643eb0160d22cf60fe1f49f2e72a1185a
Jonathan Terrell <terrell.jm@gmail.com>
2021-08-10 13:35:07 +0200

    Release v0.0.227

--- dist/nectis-observable-es.js
+++ dist/nectis-observable-es.js
@@ -1,2 +1,4 @@
-export { C as ChartJS, a as ChartPanel, D as D3XYCharts, E as ECharts, H as Highcharts, N as Nivo, T as TabPanel, b as Table, d as Theme, e as Tile, W as WorkforceSize } from './nectis-observable-index-ae290040-es.js';
-import 'react-motion';
+export { C as ChartJS, a as ChartPanel, D as D3XYCharts, E as ECharts, H as Highcharts, N as Nivo, T as TabPanel, b as Table, d as Theme, e as Tile, W as WorkforceSize } from './nectis-observable-index-100d553a-es.js';
+import '@react-spring/web';
+import '@nivo/core';
+import '@nivo/tooltip';
