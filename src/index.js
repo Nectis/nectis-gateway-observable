1dc9729e8f0802c2cb42347de67d5007f75aec2b
Jonathan Terrell <terrell.jm@gmail.com>
2021-08-10 19:07:59 +0200

    Release v0.0.250

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@
  * @license "Apache-2.0"
  */
 
-import ChartJS from './ChartJS';
-import ChartPanel from './ChartPanel';
-import D3XYCharts from './D3XYChart';
-import ECharts from './ECharts';
-import Highcharts from './Highcharts';
+// import ChartJS from './ChartJS';
+// import ChartPanel from './ChartPanel';
+// import D3XYCharts from './D3XYChart';
+// import ECharts from './ECharts';
+// import Highcharts from './Highcharts';
 import Nivo from './Nivo';
 import React from './React';
-import TabPanel from './TabPanel';
-import Table from './Table';
-import Theme from './Theme';
-import Tile from './Tile';
-import WorkforceSize from './WorkforceSize';
+// import TabPanel from './TabPanel';
+// import Table from './Table';
+// import Theme from './Theme';
+// import Tile from './Tile';
+// import WorkforceSize from './WorkforceSize';
 
 // -------------------------------------------------------------------------------------------------------------------------------
 // Exports
