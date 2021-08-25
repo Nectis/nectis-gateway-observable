654087f5d6a6ea06eaf8c6e471b45e69dca1e100
Jonathan Terrell <terrell.jm@gmail.com>
2021-08-25 13:35:01 +0200

    Release v0.0.258

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
  * @license "Apache-2.0"
  */
 
-import { Inspector, Runtime } from '@observablehq/runtime';
+// import { Inspector, Runtime } from '@observablehq/runtime';
 
 // import ChartJS from './ChartJS';
 // import ChartPanel from './ChartPanel';
@@ -29,21 +29,21 @@ const urlSuffix = '.js?v=3';
 const loadNotebook = async (notebookId, elementId) => {
     console.log(notebookId, elementId);
 
-    const notebookURL = `${urlPrefix}${notebookId}${urlSuffix}`;
-    const notebook = (await import(notebookURL)).default;
-    const presentationElement = document.getElementById(elementId);
-    const runtime = new Runtime();
-    const module = runtime.module(notebook, (name) => {
-        if (!name) return true;
-        if (name.startsWith('narrative_') || name.startsWith('visual_')) {
-            const element = document.createElement('div');
-            presentationElement.appendChild(element);
-            const inspector = new Inspector(element);
-            return inspector;
-        }
-        return true;
-    });
-    module.redefine('embedded', true);
+    // const notebookURL = `${urlPrefix}${notebookId}${urlSuffix}`;
+    // const notebook = (await import(notebookURL)).default;
+    // const presentationElement = document.getElementById(elementId);
+    // const runtime = new Runtime();
+    // const module = runtime.module(notebook, (name) => {
+    //     if (!name) return true;
+    //     if (name.startsWith('narrative_') || name.startsWith('visual_')) {
+    //         const element = document.createElement('div');
+    //         presentationElement.appendChild(element);
+    //         const inspector = new Inspector(element);
+    //         return inspector;
+    //     }
+    //     return true;
+    // });
+    // module.redefine('embedded', true);
 };
 
 // -------------------------------------------------------------------------------------------------------------------------------
